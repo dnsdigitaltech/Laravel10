08515bbddd7a2b3b3efdbe37c761b9816252c5da
Davi Bernardo <davi.bernardo@sbahq.org>
2023-12-27 15:42:23 -0300

    Como criar uma rota

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/', function () {
-    //dump($_SERVER);//mostra o log e não trava a aplicação
-    //dd($_SERVER); //mostra o log e trava a aplicação
-    //ddd($_SERVER); //mostra o log em uma tela específica com mais recursos
-    return view('welcome');
+Route::match(['get', 'post'], '/user/teste', function(){
+    dd('teste');
 });
