b7de8d58d482678a0610a0066a7a26a642333dd2
Davi Bernardo <davi.bernardo@sbahq.org>
2023-12-27 16:21:48 -0300

    Agrupando rotas

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,22 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
+Route::prefix('blog')->group(function(){
+    Route::get('/', function(){
+        dd('blog');
+    });
+
+    Route::get('/post/{slug}', function($slug){
+        dd($slug);
+    });
+});
+
+Route::group(['prefix' => 'admin'],function(){
+    Route::get('/', function(){
+        dd('admin');
+    });
+});
+
 Route::get('/', [HomeController::class, 'index']);
 
 Route::get('user/{name}/age/{age}', function($name, $age){
