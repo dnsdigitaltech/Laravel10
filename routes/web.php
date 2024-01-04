8f828f6cd78b6870eefd3d6badbc10392dea36db
Davi Bernardo <davi.bernardo@sbahq.org>
2024-01-04 20:48:32 -0300

    Middlewares nos controllers

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,13 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProtectedController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::get('/protected', [HomeController::class, 'index'])->name('protected')->middleware('teste:Davi');
+Route::get('/protected', [ProtectedController::class, 'index'])->name('protected');
+Route::get('/protected/create', [ProtectedController::class, 'create'])->name('protected.create');
 
 Route::prefix('admin')->middleware('auth')->group(function(){
     Route::get('/', [AdminController::class, 'index'])->name('admin.home')->withoutMiddleware('auth');
