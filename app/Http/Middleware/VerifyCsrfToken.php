88450e469bc417d59382b87fc58709440b97e5b4
Davi Bernardo <davi.bernardo@sbahq.org>
2024-01-04 19:43:08 -0300

    Middlewares globais

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,8 +12,5 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        '/user/12',
-        '/user',
-        '/product'
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,8 @@
 <?php
 
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ProductStoreController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\UserStoreController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/user/create', [UserController::class, 'create']);
-Route::post('/user', UserStoreController::class);
-Route::resource('/product', ProductController::class)->except('store');
-Route::post('/product', ProductStoreController::class);
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::post('/login', [LoginController::class, 'store'])->name('login');
